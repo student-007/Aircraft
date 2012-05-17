11a29da6f87d8372b0abb6f8e455a12d99d097ce
Yufei <yufei.lang@hotmail.com>
2012-05-17 11:16:54 -0400

    disabled orientation

--- Aircraft/MasterViewController.m
+++ Aircraft/MasterViewController.m
@@ -77,7 +77,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 #pragma mark - table view delegate
 
